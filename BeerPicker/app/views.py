fdcc50d62d4f429d2932052418a357acf8d68734
Levi Bibo <levibibo@outlook.com>
2016-01-14 13:25:49 -0800

    add reference to user_profile to render of home view if it is available, and keeps it as AnonymousUser if not.

--- BeerPicker/app/views.py
+++ BeerPicker/app/views.py
@@ -6,17 +6,24 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
+from .models import *
 
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
+    user = request.user
+    try:
+        user_profile = User_Profile.objects.get(id = user.id)
+    except:
+        user_profile = user
     return render(
         request,
         'app/index.html',
         context_instance = RequestContext(request,
         {
-            'title':'Home Page',
+            'title':'Beer Recommender',
             'year':datetime.now().year,
+            'user_profile':user_profile,
         })
     )
 
@@ -28,7 +35,7 @@ def about(request):
         'app/about.html',
         context_instance = RequestContext(request,
         {
-            'title':'About',
+            'title':'Beer Recommender: About',
             'message':'Your application description page.',
             'year':datetime.now().year,
         })
